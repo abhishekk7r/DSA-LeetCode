6462e06cdd5c52733bc353fe435995303b00dc60
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-08-03 08:45:23 +0530

    Time: 7 ms (19.40%), Space: 5.9 MB (76.02%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -4,15 +4,20 @@ public:
        int digit;
         int ans=0;
         while(x!=0){
+            //using modulus 10 we extract the last digit
             digit=x%10;
-             if(ans<(INT_MIN/10) || ans>(INT_MAX/10)){
+            
+            //We check whether the number ccan be stored in INT or not.
+            if(ans<(INT_MIN/10) || ans>(INT_MAX/10)){
             return 0;
-        }
+            }
+            
+            //Formula used to reverse the number
             ans=(ans*10)+digit;
+            
+            //We move to the next digit in the number from right to left
             x=x/10;
         }
-        
-            return ans;
-        
+        return ans;
     }
 };
