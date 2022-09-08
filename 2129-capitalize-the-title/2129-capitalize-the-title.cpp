4555a25b017f5d5a66976242f9f74b0b1be7b83c
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-09 01:40:29 +0530

    Time: 3 ms (67.31%), Space: 6.4 MB (43.35%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    string capitalizeTitle(string s) {
+        int n = s.size();
+        string str= "";
+        string part = "";
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            if(s[i]>='A' && s[i]<='Z'){
+                s[i]=s[i]+32;
+            }
+        }
+        
+        int i=0;
+        int j=0;
+        int k=0;
+        
+        while(k<s.size()){
+            
+            if(s[j]==' ' && (j-i)>2){
+               part = s.substr(i, (j-i));
+               part[0] = part[0] - 32; 
+               str += part; 
+               str += ' ';
+                i= j+1;
+                j++;
+                part="";
+                
+            } 
+            else if(s[j]==' ' && (j-i)<=2) 
+            {
+                str += s.substr(i, (j-i));
+                str += ' ';
+                i= j+1;
+                j++;
+                part="";
+            }
+            else{
+                j++;
+            }
+            k++;
+        }
+        
+        if((j-i)>2){
+             part = s.substr(i, (j-i));
+               part[0] = part[0] - 32; 
+               str += part; 
+               
+                i= j+1;
+                j++;
+                part="";
+                        
+
+            
+        }
+        else{
+            str += s.substr(i, (j-i));
+                
+                i= j+1;
+                j++;
+                part="";
+            
+        }
+        
+        return str;
+        
+    }
+};
