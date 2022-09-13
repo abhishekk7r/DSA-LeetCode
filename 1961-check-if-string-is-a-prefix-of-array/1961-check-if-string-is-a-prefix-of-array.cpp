4806e5b45d472856790a3819609b3d8cd8bb8c11
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-13 12:15:12 +0530

    Time: 11 ms (39.19%), Space: 14.1 MB (20.90%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     bool isPrefixString(string s, vector<string>& words) {
         string ans = "";
-        // for(int i=0; i<words.size(); i++){
-        //     ans += words[i];
-        // }
-        // if(ans.find(s)==0 && s.size()<=ans.size() ){
-        //     return 1;
-        // }
-        // return 0;
+        /*for(int i=0; i<words.size(); i++){
+            ans += words[i];
+        }
+        if(ans.find(s)==0 && s.size()<=ans.size() ){
+            return 1;
+        }
+        return 0;*/
+        
+        //jab ans == s ho jayega tabhi return 1 hoga nahi to add hote jayega ans me words[i]
+        //add karne k baad bhi equal nahi aya to return 0 ho jayega
         for(int i=0; i<words.size(); i++){
             ans += words[i];
             if(ans == s)
