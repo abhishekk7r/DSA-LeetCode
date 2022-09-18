dfa852fcc9ef55eaf3747b8805c2b7b1ebf98ba9
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-19 03:59:54 +0530

    Time: 770 ms (7.21%), Space: 10.3 MB (70.12%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     int countPrimes(int n) {
         int count = 0;
+        
+        //using steve of eratostene
+        
+        //Mark all numbers as prime number
         vector<bool> prime(n+1, true);
         
+        //mark 0 and 1 as non prime
         prime[0] = prime[1] = 0;
         
+        //check whether i is prime or not
         for(int i=2; i<n; i++)
         {
             if(prime[i])
                 count++;
             
+            //if prime then mark all number in it's table as non prime
             for(int j=2*i; j<n; j=j+i)
             {
                 prime[j]=0;
