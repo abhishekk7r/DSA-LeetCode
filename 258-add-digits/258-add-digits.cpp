38ef25733a88bc9f9ef1f21df76ec8dcfb4e2800
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-22 14:38:56 +0530

    Time: 8 ms (13.59%), Space: 5.9 MB (25.48%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int addDigits(int num) {
-
+        //loop till the number is not a single digit number
         while(num>9){
-            int temp = num%10; 
-            num = num/10 + temp; 
+            int temp = num%10; //store last digit into temp
+            num = num/10 + temp; //add the temp into num divided by 10
         }
-        
         return num;
     }
 };
