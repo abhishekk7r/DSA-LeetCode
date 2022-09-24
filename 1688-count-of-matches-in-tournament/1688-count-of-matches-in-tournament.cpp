904120593662243f6d7dab35d7ac90d691a4c2cd
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-24 23:15:52 +0530

    Time: 3 ms (37.92%), Space: 5.8 MB (72.20%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-//         int matches = n/2;
-//         int sum = 0;
-//         while(matches!=1)
-//         {
-//             sum += matches;
-//             matches = n/2;
-//             if(n%2!=0)
-//                 n = matches + 1;
-//             else 
-//                 n = matches;
-            
-//         }
-//         return sum;
+        //no of matches will always be equal to the n-1;
         return n-1;
     }
 };
