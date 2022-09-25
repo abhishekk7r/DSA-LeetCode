a520d518c78051f70f572c938f83cd017da32dd7
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-26 00:32:29 +0530

    Time: 16 ms (69.82%), Space: 10.4 MB (20.69%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
-    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector <int> v(nums.size(), 0);
-        
-        for(int i=0; i<nums.size(); i++){
-            
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {        
+        /*vector <int> v; 
+            for(int i=0; i<nums.size(); i++){
             for(int j = 0; j<nums.size(); j++){
-                
                 int ans = nums[i];
                 if(ans>nums[j])
-                    v[i]++;
-                
+                    v[i]++;   
             }
-            
+        }*/
+        
+        vector <int> ans = nums;
+        int n = nums.size();
+        map <int, int> m;
+        
+        sort(ans.begin(), ans.end());
+        
+        //map me chala gaya 
+        // T.C o(n)
+        for(int i=n-1; i>=0; i--)
+        {
+            m[ans[i]] = i;
         }
-        return v;
+        
+        
+        //map me find karege ki kaha pe hai mera element and uska value kya hai
+        for(int i=0; i<n; i++)
+        {
+            nums[i] = m[nums[i]];
+        }
+        
+        return nums;
+        
+        
+        
+        
+        
     }
 };
