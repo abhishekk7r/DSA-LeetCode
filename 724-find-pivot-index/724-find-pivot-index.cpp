ddeadea4bdba66c27b7d167806bfddc90a98aa25
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2022-09-29 07:15:18 +0530

    Update 724-find-pivot-index.cpp

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        long int tSum = 0; int leftSum=0; int rightSum=0;
-        int ans=-1;
+        int ans = -1; 
         
-        for(int i =0 ; i<n; i++){
-            tSum += nums[i];
+        long long totalSum = 0;
+        int leftSum = 0, rightSum = 0;
+        
+        int n = nums.size();
+        
+        //first we store the total sum in total sum variable
+        for(int i =0; i<n; i++)
+        {
+            totalSum += nums[i];
         }
-        rightSum = tSum; 
-        for(int i=0; i<n; i++){
-              rightSum = rightSum-nums[i];
-            if(leftSum == rightSum){
+        
+        /*Just for refrencing we calculate the totalSum and then assign it to rightSum 
+        variable */
+        rightSum = totalSum;
+        
+        
+        /*For each i we subtract nums[i] element from the totalSum and check whether
+        it is equal to the leftSum. If it is not equal we add nums[i] to the 
+        leftSum and Check Again*/
+        for(int i=0 ; i<n; i++)
+        {
+            rightSum = rightSum - nums[i]; 
+            
+            if(rightSum == leftSum){
                 ans = i;
                 break;
             } else {
                 leftSum += nums[i];
-                
-            } 
+            }
         }
+        
         return ans;
     }
 };
