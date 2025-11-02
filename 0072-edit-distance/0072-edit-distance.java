d1216f8ce4efec1e239fd314fd7c7837e7ad7852
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2025-11-02 23:23:03 +0530

    Time: 4 ms (71.16%), Space: 46.2 MB (6.16%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -28,31 +28,41 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] memo = new int[n + 1][m + 1];
+        /*int[][] memo = new int[n + 1][m + 1];
 
         // for(int i=0; i<n; i++) Arrays.fill(memo[i], -1);
         //return f(n-1, m-1, word1, word2, memo);   
 
         for (int i = 0; i <= n; i++)
             memo[i][0] = i;
-        for (int j = 0; j <= m; j++)
-            memo[0][j] = j;
+        
         
         memo[0][0] = 0;
+        */
+        
+        int [] curr = new int[m + 1];
+        int [] prev = new int[m + 1];
+
+        for (int j = 0; j <= m; j++) prev[j] = j;
 
         for (int i = 1; i <= n; i++) {
+            curr[0] = i;
             for (int j = 1; j <= m; j++) {
                 char a = word1.charAt(i - 1);
                 char b = word2.charAt(j - 1);
 
                 if (a == b) {
-                    memo[i][j] = memo[i - 1][j - 1];
+                    curr[j] = prev[j-1];
                 } else {
-                    memo[i][j] = 1 + Math.min(memo[i - 1][j], Math.min(memo[i][j - 1], memo[i - 1][j - 1]));
+                    //memo[i][j] = 1 + Math.min(memo[i - 1][j], Math.min(memo[i][j - 1], memo[i - 1][j - 1]));
+                    curr[j] = 1 + Math.min(prev[j], Math.min(curr[j-1], prev[j-1]));
                 }
+
+                
             }
+            prev = Arrays.copyOf(curr, curr.length);
         }
 
-        return memo[n][m];
+        return prev[m];
     }
 }
