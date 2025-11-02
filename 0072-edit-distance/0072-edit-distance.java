810e329ccc633397931058f710f4e2be51839f7a
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2025-11-02 23:05:12 +0530

    Time: 5 ms (57.32%), Space: 47.3 MB (6.16%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,58 @@
+class Solution {
+    private int f(int i, int j, String word1, String word2, int[][] memo) {
+
+        //If string 1 runs out
+        if (i < 0)
+            return j + 1;
+
+        //If string 2 runs out
+        if (j < 0)
+            return i + 1;
+
+        if (memo[i][j] != -1)
+            return memo[i][j];
+
+        //If Both S1[i] & S2[j] matches
+        //We can move the i & j together
+        if (word1.charAt(i) == word2.charAt(j)) {
+            return f(i - 1, j - 1, word1, word2, memo);
+        }
+
+        //Insert -> Move J, as we have matched with inserted character
+        //Delete -> Move I, as we have deleted one charð
+        return 1 + Math.min(f(i - 1, j, word1, word2, memo),
+                Math.min(f(i, j - 1, word1, word2, memo), f(i - 1, j - 1, word1, word2, memo)));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] memo = new int[n + 1][m + 1];
+
+        // for(int i=0; i<n; i++) Arrays.fill(memo[i], -1);
+        //return f(n-1, m-1, word1, word2, memo);   
+
+        for (int i = 0; i <= n; i++)
+            memo[i][0] = i;
+        for (int j = 0; j <= m; j++)
+            memo[0][j] = j;
+        
+        memo[0][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                char a = word1.charAt(i - 1);
+                char b = word2.charAt(j - 1);
+
+                if (a == b) {
+                    memo[i][j] = memo[i - 1][j - 1];
+                } else {
+                    memo[i][j] = 1 + Math.min(memo[i - 1][j], Math.min(memo[i][j - 1], memo[i - 1][j - 1]));
+                }
+            }
+        }
+
+        return memo[n][m];
+    }
+}
