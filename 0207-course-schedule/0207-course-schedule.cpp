f98211b4e6dc0180ceaca5ff81c5c4c17f20e56d
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2026-08-23 19:26:46 +0530

    Time: 9 ms (27.58%), Space: 21.1 MB (6.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
+        unordered_map<int, vector<int>> mp;
         vector<int> indegree(numCourses);
 
-        //Create edges to a node & find indegree of every node
-        for(auto &i:prerequisites){
-            graph[i[1]].push_back(i[0]);
+        for(auto i:prerequisites){
+            mp[i[1]].push_back(i[0]);
             indegree[i[0]]++;
         }
 
-        //Create Queue
-        //Add those elements whoes indegree is 0
         queue<int> q;
-        
         for(int i=0; i<numCourses; i++){
             if(indegree[i] == 0) q.push(i);
         }
 
-        //Create answer array
-        int count = 0;
-
-
-        //Process those nodes whose indegree is 0,
-        //redeuce the indegree of those node who are connected to current node.
-        //as we are removing one edge
-        //If indegree becomes 0, add them
+        vector<int> ans;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            count++;
-            for(int curr:graph[node]){
-                indegree[curr]--;
-                if(indegree[curr] == 0) q.push(curr);
+
+            for(auto &i:mp[node]){
+                indegree[i]--;
+
+                if(indegree[i] == 0){
+                    q.push(i);
+                }
             }
+
+            ans.push_back(node);
         }
 
-        //if size of answer == numsCourses this means we can take all course
-        return count == numCourses;
+        return (ans.size() == numCourses);
     }
 };
